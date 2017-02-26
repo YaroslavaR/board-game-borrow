351234cac903740e98458648846a9b1f1b11c422
Yaroslava Romanets <yaroslava.romanets@yahoo.com>
2017-02-26 22:24:32 +0100

    Improving init script

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 bundle install
 bin/rails db:migrate RAILS_ENV=development
-echo "insert into users (email,name,is_admin,created_at,updated_at,encrypted_password) values ('admin@admin.ad','admin',1,'2017-02-25 20:47:38.749339','2017-02-25 20:47:38.749339','$2a$11$J1.wd62oHVO/o9DdeToJreMsaXatjivfI9bwFfG4M5G47Zzg2t4Om');" > create_admin.sql
-sqlite3 db/development.sqlite3 < create_admin.sql
-rm create_admin.sql
+rake:db seed
 bin/rails server
